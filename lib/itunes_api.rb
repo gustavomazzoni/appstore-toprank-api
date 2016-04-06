0b6a60c5678c102f7e0def66699c7e7ad752804e
Gustavo Mazzoni <gustavo.mazzoni@gmail.com>
2016-04-06 16:51:40 -0300

    Created class that handles requests for iTunes API methods.

---
+++ lib/itunes_api.rb
@@ -0,0 +1,23 @@
+require 'httparty'
+
+class ITunesAPI
+	include HTTParty
+	base_uri 'https://itunes.apple.com'
+	
+  def view_top(query = {}, headers = {})
+    raise ArgumentError, 'You must inform a genreId' if query[:genreId].nil?
+
+    # make the request and return the elements (NOTE: this doesn't handle errors at this point)
+    self.class.get('/WebObjects/MZStore.woa/wa/viewTop', headers: headers, query: query)
+  end
+
+  def lookup(query = {})
+    raise ArgumentError, 'You must lookup for something' if query.empty?
+    self.class.get("/lookup", :query => query)["results"]
+  end
+
+  def search(query = {})
+    raise ArgumentError, 'You must search for something' if query.empty?
+    self.class.get("/search", :query => query)["results"]
+  end
+end
