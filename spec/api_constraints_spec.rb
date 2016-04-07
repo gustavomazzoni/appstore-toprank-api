1cfb09cf66260e1f61089986e5850eeae26d07a8
Gustavo Mazzoni <gustavo.mazzoni@gmail.com>
2016-04-07 14:22:38 -0300

    Updated

--- spec/api_constraints_spec.rb
+++ spec/api_constraints_spec.rb
@@ -1,19 +1,19 @@
 require 'api_constraints'
 
-describe ApiConstraints do
+RSpec.describe ApiConstraints do
   let(:api_constraints_v1) { ApiConstraints.new(version: 1) }
   let(:api_constraints_v2) { ApiConstraints.new(version: 2, default: true) }
 
   describe "matches?" do
 
     it "returns true when the version matches the 'Accept' header" do
-      request = double(host: 'api.appstore-info',
+      request = double(host: 'api.appstoreinfo',
                        headers: {"Accept" => "application/vnd.appstore-info.v1"})
       expect(api_constraints_v1.matches?(request)).to be_truthy
     end
 
     it "returns the default version when 'default' option is specified" do
-      request = double(host: 'api.appstore-info')
+      request = double(host: 'api.appstoreinfo')
       expect(api_constraints_v2.matches?(request)).to be_truthy
     end
   end
