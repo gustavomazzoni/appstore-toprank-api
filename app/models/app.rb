ad13f8cdb90058610af21c7ec93f50fda60d6ffa
Gustavo Mazzoni <gustavo.mazzoni@gmail.com>
2016-04-08 16:42:21 -0300

    Added Cache to reduce API calls.

--- app/models/app.rb
+++ app/models/app.rb
@@ -7,38 +7,40 @@ class App
   @@itunes_api = ITunesAPI.new
 
   def self.find_by_id(id)
-  	app_metadata = @@itunes_api.lookup(:id => id)
+  	Rails.cache.fetch("/app/#{id}/find_by_id", :expires_in => 1.day) do
+	  	app_metadata = @@itunes_api.lookup(:id => id)
 
-  	raise ArgumentError, 'Invalid id argument' if app_metadata.nil?
-	
-	# Create app with metadata
-	if !app_metadata.empty?
-		app = App.new(
-			:appId => id,
-			:appName => app_metadata.first["trackName"],
-			:description => app_metadata.first["description"],
-			:smallIconUrl => app_metadata.first["artworkUrl60"],
-			:publisherName => app_metadata.first["sellerName"],
-			:price => app_metadata.first["formattedPrice"],
-			:versionNumber => app_metadata.first["version"],
-			:averageUserRating => app_metadata.first["averageUserRating"]
-		)
-	# In case app not found on lookup, 
-	# create app with empty metadata
-	else 
-		app = App.new(
-			:appId => id,
-			:appName => "",
-			:description => "",
-			:smallIconUrl => "",
-			:publisherName => "",
-			:price => "",
-			:versionNumber => "",
-			:averageUserRating => ""
-		)
+	  	raise ArgumentError, 'Invalid id argument' if app_metadata.nil?
+		
+		# Create app with metadata
+		if !app_metadata.empty?
+			app = App.new(
+				:appId => id,
+				:appName => app_metadata.first["trackName"],
+				:description => app_metadata.first["description"],
+				:smallIconUrl => app_metadata.first["artworkUrl60"],
+				:publisherName => app_metadata.first["sellerName"],
+				:price => app_metadata.first["formattedPrice"],
+				:versionNumber => app_metadata.first["version"],
+				:averageUserRating => app_metadata.first["averageUserRating"]
+			)
+		# In case app not found on lookup, 
+		# create app with empty metadata
+		else 
+			app = App.new(
+				:appId => id,
+				:appName => "",
+				:description => "",
+				:smallIconUrl => "",
+				:publisherName => "",
+				:price => "",
+				:versionNumber => "",
+				:averageUserRating => ""
+			)
+		end
+
+		app
 	end
-
-	return app
   end
 
 end
--- app/models/top_rank.rb
+++ app/models/top_rank.rb
@@ -3,6 +3,7 @@ require 'future'
 
 class TopRank
   include ActiveModel::Model
+ 
   attr_accessor :title, :categoryName, :apps, :topPublishers
   @@itunes_api = ITunesAPI.new
   @@headers = {"Accept­Encoding" => "gzip, deflate, sdch",
@@ -12,29 +13,44 @@ class TopRank
 				"Cache­Control" => "max­age=0",
 				"X­Apple­Store­Front" => "143441­1,17"}
 
+
+ 	#  	def Product.out_of_stock
+	# 	Rails.cache.fetch("out_of_stock_products", :expires_in => 5.minutes) do
+	# 		Product.all.joins(:inventory).conditions.where("inventory.quantity = 0")
+	# 	end
+	# end
+
+	# def competing_price
+	# 	Rails.cache.fetch("/product/#{id}-#{updated_at}/comp_price", :expires_in => 12.hours) do
+	# 		Competitor::API.find_price(id)
+	# 	end
+	# end
+
   def self.find(genreId, monetization, popId)
-  	top_rank_response = @@itunes_api.view_top({genreId:genreId, dataOnly: true, l: 'en'}, @@headers)
+  	Rails.cache.fetch("/top_rank/#{genreId}-#{monetization}-#{popId}/find", :expires_in => 4.hours) do
+  		top_rank_response = @@itunes_api.view_top({genreId:genreId, dataOnly: true, l: 'en'}, @@headers)
 
-	#get category name
-	category_name = top_rank_response['genre']['name']
-	#get top rank list (Top Paid iPhone Apps, Top Free iPhone Apps, Top Grossing iPhone Apps)
-	top_charts = top_rank_response['topCharts']
-	
-	#get top rank from monetization chosen by user
-	top_chart = top_charts.select {|hash| hash['shortTitle'] == monetization}
-	
-	#initialize the TopRank object to be returned
-	top_rank = TopRank.new(
-		:title => top_chart[0]['title'],
-		:categoryName => category_name,
-		:apps => top_chart[0]['adamIds']
-	)
+		#get category name
+		category_name = top_rank_response['genre']['name']
+		#get top rank list (Top Paid iPhone Apps, Top Free iPhone Apps, Top Grossing iPhone Apps)
+		top_charts = top_rank_response['topCharts']
+		
+		#get top rank from monetization chosen by user
+		top_chart = top_charts.select {|hash| hash['shortTitle'] == monetization}
+		
+		#initialize the TopRank object to be returned
+		top_rank = TopRank.new(
+			:title => top_chart[0]['title'],
+			:categoryName => category_name,
+			:apps => top_chart[0]['adamIds']
+		)
 
-	return top_rank
+		top_rank
+	end
   end
 
   def self.find_with_metadata(genreId, monetization, popId)
-  	top_rank = TopRank.find(genreId, monetization, nil)
+  	top_rank = TopRank.find(genreId, monetization, popId)
 
 	#get additional metadata information for each adamIds via Apple lookup API
 	top_rank.apps = top_rank.apps.map{ |adam_id| 
@@ -55,46 +71,49 @@ class TopRank
   	return App.find_by_id(appId)
   end
 
-  def self.find_top_publishers(genreId, monetization)
-  	top_rank = TopRank.find(genreId, monetization, nil)
+  def self.find_top_publishers(genreId, monetization, popId)
+  	top_rank = TopRank.find(genreId, monetization, popId)
 
   	#future list with top publishers with info
 	top_publishers = []
-	#get additional metadata information for each adamIds via Apple lookup API
-	top_rank.apps.each do |app_id|
-		#create parallel calls
-		future {
-		  	app_metadata = @@itunes_api.lookup(:id => app_id)
-						
-			# Create publisher with infos
-			if !app_metadata.empty?
-				#check if this publisher already in rank list
-				publisherItem = top_publishers.select {|hash| hash.publisherId == app_metadata.first["artistId"]}
-				if publisherItem.empty?
-					publisher = Publisher.new(
-						:publisherId => app_metadata.first["artistId"],
-						:publisherName => app_metadata.first["artistName"],
-						:numberOfApps => 1,
-						:apps => [app_metadata.first["trackName"]]
-					)
-					#add publisher to the list
-					top_publishers.push(publisher)
-				else 
-					publisherItem[0].numberOfApps += 1
-					publisherItem[0].apps.push(app_metadata.first["trackName"])
+
+	Rails.cache.fetch("/top_rank/#{genreId}-#{monetization}-#{popId}/find_top_publishers", :expires_in => 4.hours) do
+		#get additional metadata information for each adamIds via Apple lookup API
+		top_rank.apps.each do |app_id|
+			#create parallel calls
+			future {
+			  	app_metadata = @@itunes_api.lookup(:id => app_id)
+							
+				# Create publisher with infos
+				if !app_metadata.empty?
+					#check if this publisher already in rank list
+					publisherItem = top_publishers.select {|hash| hash.publisherId == app_metadata.first["artistId"]}
+					if publisherItem.empty?
+						publisher = Publisher.new(
+							:publisherId => app_metadata.first["artistId"],
+							:publisherName => app_metadata.first["artistName"],
+							:numberOfApps => 1,
+							:apps => [app_metadata.first["trackName"]]
+						)
+						#add publisher to the list
+						top_publishers.push(publisher)
+					else 
+						publisherItem[0].numberOfApps += 1
+						publisherItem[0].apps.push(app_metadata.first["trackName"])
+					end
 				end
-			end
-		}
+			}
 
+		end
+		#wait for the parallel calls to finish
+		sleep 9
+		
+		#sort top publishers list by number of apps they have on top rank
+		top_publishers = top_publishers.sort_by {|hash| hash.numberOfApps}.reverse!
+		#add rank property to each element of the list with publisher's position in the rank
+		top_publishers.map.with_index{ |hash, i| hash.rank = i+1 }
+
+		top_publishers
 	end
-	#wait for the parallel calls to finish
-	sleep 9
-	
-	#sort top publishers list by number of apps they have on top rank
-	top_publishers = top_publishers.sort_by {|hash| hash.numberOfApps}.reverse!
-	#add rank property to each element of the list with publisher's position in the rank
-	top_publishers.map.with_index{ |hash, i| hash.rank = i+1 }
-
-	return top_publishers
   end
 end
