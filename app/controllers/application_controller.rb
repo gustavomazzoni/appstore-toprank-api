a7e4aa0cfde5a8d105e43d2daabc79eb0d91988e
Gustavo Mazzoni <gustavo.mazzoni@gmail.com>
2016-04-08 18:26:07 -0300

    Added rescue ArgumentError exception for all actions to return bad_request to user.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
+
+  rescue_from ArgumentError, with: :invalid_argument
+ 
+  private
+ 
+    def invalid_argument
+      render nothing: true, status: :bad_request
+    end
 end
